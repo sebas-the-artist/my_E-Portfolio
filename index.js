5e3ce890ede2801ae313b9f4ba987533712aa37e
sebastian <manijustworkhere@gmail.com>
2025-08-17 16:59:41 -0400

    i was gonna have way more shapes but its not too 'forgiving' given that the video i watched half assed the tutorial so 9 it is also making it responsive

--- index.js
+++ index.js
@@ -2,7 +2,24 @@
 //service_ka386sl
 //xSSG4mKNPS7AGUiDA      UMoRBjsiN_M9CU_7m
 
-let contrastToggle
+let contrastToggle = false
+const scaleFactor = 1 / 20
+
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+        shapes[i].style.transform = `rotate(${x}deg, ${y}deg)`
+    }
+}
+
+
 
 function toggleContrast() {
     contrastToggle = !contrastToggle;
